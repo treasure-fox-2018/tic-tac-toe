2a763b0ab5463e5f37c87cb12578bacf6e5be5f3
Ari Supriatna <arisupriatna@fabio-Aspire-E5-476G.localdomain>
2018-05-30 16:45:13 +0700

    tic-tac-toe done

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-function randomTicTacToe(x, o) {
+function randomTicTacToe(board) {
   const tictactoe = 'XO'
-  let hurufX = 4
-  let hurufO = 4
-  const random = tictactoe[Math.floor(Math.random() * 2)]
-  let arrTicTacToe = []
+  let gameBoardTicTacToe = []
 
-  for (let a = 0; a < 3; a++) {
-    if (random === 'X') {
-      arrTicTacToe.push('X')
-      hurufX -= 1
-    } else if (random === 'O'){
-      arrTicTacToe.push('O')
-      hurufO -= 1
+  for (let a = 0; a < board; a++) {
+    var isiBoard = []
+    for (let b = 0; b < board; b++) {
+      isiBoard.push(tictactoe[Math.floor(Math.random() * 2)])
+    }
+    gameBoardTicTacToe.push(isiBoard)
+  }
+  return gameBoardTicTacToe
+}
+
+function gameTicTacToe(board) {
+  let hurufX = 0
+  let hurufO = 0
+  let game = randomTicTacToe(3)
+  for (let a = 0; a < game.length; a++) {
+    for (let b = 0; b < game[a].length; b++) {
+      if (game[a][b] === 'X') {
+        hurufX++
+        if (hurufX > 5) {
+          game[a][b] = 'O'
+        }
+      } else if (game[a][b] === 'O') {
+        hurufO++
+        if (hurufO > 5) {
+          game[a][b] = 'X'
+        }
+      }  
     }
   }
 
-  return arrTicTacToe
-
-
+  console.log(game)
 }
-
-console.log(randomTicTacToe(5, 4))
+gameTicTacToe()
