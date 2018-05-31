07754a5f38e107fe0960a0824be8309899742008
HelmiYogantara <helmiyogantara@gmail.com>
2018-05-31 10:27:01 +0700

    tambah kondisi check yang keluar dan bagaimana menyikapinya

--- index.js
+++ index.js
@@ -9,39 +9,30 @@ function ticTacToe(numRows, numCols){
         for(var j = 0; j < numCols; j++){
             check = xo[Math.floor(Math.random()*2)]     // j = 0; x x= 1        child = x x
             if(check === 'X'){                          // j = 1; x x = 2
-                x++                                     // j = 2; 0 0 = 1       child = x x o
-            }else {
+                x++ 
+                if(x <=5){
+                    childArr.push(check)
+                }else{
+                    childArr.push('O')
+                    o++
+                    x--
+                }                      
+            }else if(check === 'O'){
                 o++
+                if(o <= 5){
+                    childArr.push(check)
+                }else{
+                    childArr.push('X')
+                    x++
+                    o--
+                }
             }
-
-            if(check === 'O' && o <= 3){
-                childArr.push(check)
-            }else if(check === 'X' && x <= 3){
-                childArr.push(check)
-            }
-            
         }
         mainArr.push(childArr)
         childArr = []
-        o = 0
-        x = 0
 
     }
-
-    for(var i = 0; i < mainArr.length; i++){
-        for(var j = 0; j < mainArr[i].length;j++)
-        if(mainArr[i][j] === 'X'){
-            x++
-            if( x >= 6){
-                mainArr[i][j] = 'O'
-            }
-        }else if(mainArr[i][j] === 'O'){
-            o++
-            if( o >= 6){
-                mainArr[i][i] = 'X'
-            }
-        }
-    }
+    
     return mainArr
     
 }
